1edc82b748485b9403d1fe0932ac3c97c5b7a311
CharlyAuzanet <charly.auzanet@gmail.com>
2017-03-01 14:23:12 +0100

    Placement des différentes div en php (header, tarif et footer)

---
+++ img/tarif.php
@@ -0,0 +1,60 @@
+<!-- Bureau version -->
+	<section id="c_tarif" class="row">
+		<div id="c_tarif-container" class="col-xs-offset-3 col-xs-6">
+			<div id="c_vélo" class="col-xs-6">
+				<ul>
+					<li><h3>Tarif vélo :</h3></li>
+					<li>8€ l'heure / vélo</li>
+					<li>15€ la journée / vélo</li>
+					<li>55€ la journée / vélo</li>
+					<li>100€ les 2 semaines / vélo</li>
+					<li>130€ le mois / vélo</li>
+				</ul>
+			</div>
+			<div id="c_acces" class='col-xs-6'>
+				<ul>
+					<li><h3>Tarif accessoires :</h3></li>
+					<li>Casque : 3€</li>
+					<li>Lot de 2 gourdes : 4€</li>
+					<li>Porte bébé : 5€</li>
+					<li>Plan du site et des différents parcours : 8€</li>
+				</ul>
+			</div>
+			<div id="c_infoloc" class='col-xs-offset-2 col-xs-8'>
+				<p>Chaque location, quelqu'en soit la durée, fera l'objet d'un cautionnement soit par le dépôt d'un passeport, d'une CNI ou permis de conduire, restitué à la fin de la location ; soit par le dépôt d'une carte bancaire de caution de 200 euros, restitué à la fin de la location.<br/><br/>
+
+				Le locataire doit être couvert par son assurance civile.<br/><br/>
+				Découvrez Fontainebleau et sa forêt sur le site de l'office de tourisme de Fontainebleau : www.fontainebleau-tourisme.com</p>
+			</div>
+		</div>
+	</section>
+<!-- Mobile version -->
+	<section id="c_tarif_mobile">
+		<div class="row">
+			<div class="col-sm-6">	
+				<ul>
+					<li><h3>Tarif vélo :</h3></li>
+					<li>8€ l'heure / vélo</li>
+					<li>15€ la journée / vélo</li>
+					<li>55€ la journée / vélo</li>
+					<li>100€ les 2 semaines / vélo</li>
+					<li>130€ le mois / vélo</li>
+				</ul>
+			</div>
+			<div class="col-sm-6">
+				<ul>
+					<li><h3>Tarif accessoires :</h3></li>
+					<li>Casque : 3€</li>
+					<li>Lot de 2 gourdes : 4€</li>
+					<li>Porte bébé : 5€</li>
+					<li>Plan du site et des différents parcours : 8€</li>
+				</ul>
+			</div>
+		</div>
+		<div>			
+			<p>Chaque location, quelqu'en soit la durée, fera l'objet d'un cautionnement soit par le dépôt d'un passeport, d'une CNI ou permis de conduire, restitué à la fin de la location ; soit par le dépôt d'une carte bancaire de caution de 200 euros, restitué à la fin de la location.<br/><br/>
+
+			Le locataire doit être couvert par son assurance civile.<br/><br/>
+			Découvrez Fontainebleau et sa forêt sur le site de l'office de tourisme de Fontainebleau : www.fontainebleau-tourisme.com</p>
+		</div>	
+	</section>
