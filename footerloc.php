38ef4d2d1538355d878f346e8b5b29c35cf69661
CharlyAuzanet <charly.auzanet@gmail.com>
2017-03-03 09:36:03 +0100

    Fix opacité barre de nav + add zone email footer loc

--- footerloc.php
+++ footerloc.php
@@ -7,7 +7,7 @@
 					<div class="col-xs-2">VILLE DE FONTAINEBLEAU<a href="http://www.fontainebleau.fr/" target="_blank"><img  src="img/villedefontainebleau.jpg" div="c_img2" class="img-responsive" alt="villedebleau"></a></div>
 					<div class="col-xs-2">FÉDÉRATION FRANÇAISE DE VTT<a href="http://www.ffc.fr/discipline/vtt/" target="_blank"><img  src="img/ffv.png" div="c_img3" class="img-responsive" alt="villedebleau"></a></div>
 					<div class="col-xs-2">MONITEUR VTT FONTAINEBLEAU<a href="http://www.moniteurcycliste.com/77-seine-et-marne/ecole-mcf-du-massif-de-fontainebleau" target="_blank"><img  src="img/mcf.png" div="c_img4" class="img-responsive" alt="villedebleau"></a></div>
-					<div id="c_formualire" class="col-xs-4">FORMULAIRE DE CONTACT :<br/><br/><textarea placeholder="Envoyez votre question içi..." style="width:100%" rows="5"></textarea><br/><input type="submit" value="Envoyer" /></div>		
+					<div id="c_formualire" class="col-xs-4">FORMULAIRE DE CONTACT :<br/><br/><textarea placeholder="Votre e-mail..." style="width:50% height:"rows="1"></textarea><textarea placeholder="Envoyez votre question içi..." style="width:100%" rows="5"></textarea><br/><input type="submit" value="Envoyer" /></div>		
 				</div>
 			</div>
 		</div>	
@@ -24,10 +24,10 @@
 			<div><a href="http://www.fontainebleau.fr/" target="_blank"><h4>VILLE DE FONTAINEBLEAU</h4></a></div>
 			<div><a href="http://www.ffc.fr/discipline/vtt/" target="_blank"><h4>FÉDÉRATION FRANÇAISE DE VTT</h4></a></div>
 			<div><a href="http://www.moniteurcycliste.com/77-seine-et-marne/ecole-mcf-du-massif-de-fontainebleau" target="_blank"><h4>MONITEUR VTT FONTAINEBLEAU</h4></a></div><br/><br/>
-			<div id="c_formualire"><h4>FORMULAIRE DE CONTACT :</h4><textarea placeholder="Envoyez votre question içi..." style="width:100%" rows="5"></textarea><br/></div>
+			<div id="c_formualire"><h4>FORMULAIRE DE CONTACT :</h4><textarea placeholder="Votre e-mail..." style="width:100%" rows="1"></textarea><textarea placeholder="Envoyez votre question içi..." style="width:100%" rows="5"></textarea><br/></div>
 			<div class="row">
-			<input class="col-xs-offset-4 col-xs-4" id="boutonm" type="submit" value="Envoyer" /></div>
-		</div>
+				<input class="col-xs-offset-4 col-xs-4" id="boutonm" type="submit" value="Envoyer" /></div>
+			</div>
 		<div class="row">
 			<div id="c_copyright_m" class="col-xs-offset-1 col-xs-10">
 				<p>&copy;WCS Fontainebleau</p>
